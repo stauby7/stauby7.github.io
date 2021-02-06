409ebe8bef2d8d30580454fb21c292288aeb0389
Istaub23 <stauby7@gmail.com>
2021-02-06 03:43:24 +0000

    $ git commit -m "render pyramid to the DOM instead of the console"

--- mario.js
+++ mario.js
@@ -1,16 +1,16 @@
-let height = window.prompt("How tall would you like the pyramid?");
+let height = window.prompt("How tall is your pyramid?")
 function createPyramid (height) {
   
-  for (var i = 1; i <=height; i++) {
-    var row = "</br>";
+  for (var i = 1; i <= height; i++) {
+    var row = '';
     
-    for (var j = 1; j <= height - i; j++) {
-      row += '.';
+    for (var j = 1; j <= (height - i); j++) {
+      row += ' ';
     }
     for (var k = 1; k <= i; k++) {
       row += '#';
     }
-      document.getElementById('pyramid').innerHTML += row
+     console.log(row);
   }
 }
 createPyramid(height);
